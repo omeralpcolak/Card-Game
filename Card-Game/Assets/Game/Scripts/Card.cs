9fa6e9a8273532fea532c29e03aaf22d5b8497de
omeralpcolak <omer.alp.colak@gmail.com>
2024-07-22 18:56:00 +0300

    Update on card script

--- Card-Game/Assets/Game/Scripts/Card.cs
+++ Card-Game/Assets/Game/Scripts/Card.cs
@@ -13,8 +13,10 @@ public class Card : MonoBehaviour
 {
     public int id;
     public CardState state;
+    public Level owner;
 
     private bool canBeFlipped;
+    [HideInInspector]public  bool canBeIntrectable = true;
 
 
     private void Start()
@@ -25,6 +27,10 @@ public class Card : MonoBehaviour
 
     private void OnMouseDown()
     {
+        if (!canBeIntrectable)
+        {
+            return;
+        }
         Flip();
     }
 
@@ -34,14 +40,42 @@ public class Card : MonoBehaviour
         {
             return;
         }
+
+        if (owner.selectedCards.Contains(this))
+        {
+            FlipMovement();
+            return;
+        }
+        else
+        {
+            owner.SelectCard(this);
+        }
+
+        FlipMovement();
+    }
+
+    public void FlipMovement()
+    {
         canBeFlipped = false;
         state = state == CardState.OPEN ? CardState.CLOSED : CardState.OPEN;
 
-        transform.DOMoveZ(-1, 0.3f).SetEase(Ease.Linear).ChangeStartValue(new Vector3(0,0,0));
-        transform.DORotate(new Vector3(0,transform.eulerAngles.y + 180f,0), 0.3f);
+        transform.DOMoveZ(-1, 0.3f).SetEase(Ease.Linear).ChangeStartValue(new Vector3(0, 0, 0));
+        transform.DORotate(new Vector3(0, transform.eulerAngles.y + 180f, 0), 0.3f);
         transform.DOMoveZ(0, 0.3f).SetDelay(0.3f).SetEase(Ease.Linear);
 
-        DOVirtual.DelayedCall(0.3f, () => canBeFlipped = true);
+        DOVirtual.DelayedCall(0.5f, () =>
+        {
+            canBeFlipped = true;
+        });
+    }
+
+    public void Dissolve()
+    {
+        transform.DOScale(Vector3.zero, 0.5f).SetEase(Ease.InBack).OnComplete(() =>
+        {
+            gameObject.SetActive(false);
+            owner.CheckLevelState();
+        });
         
     }
 
